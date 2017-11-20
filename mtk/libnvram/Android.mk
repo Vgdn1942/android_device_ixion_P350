fceb68701fc8b471f98ccdd8f482f92c9bc3f963
vgdn1942 <vgdn1942@gmail.com>
2017-11-20 12:29:12 +0300

    Purge unused lib

--- mtk/libnvram/Android.mk
+++
@@ -1,8 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-include $(CLEAR_VARS)
-LOCAL_MODULE := libnvram
-LOCAL_SRC_FILES := libnvram.so
-LOCAL_SHARED_LIBRARIES := libcustom_nvram libnvram_platform libnvram_sec libstdc++
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-LOCAL_MODULE_SUFFIX := .so
-include $(BUILD_PREBUILT)

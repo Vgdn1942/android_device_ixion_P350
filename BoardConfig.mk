f2a20c5e36a474260e1a7b81926894410b011aae
vgdn1942 <vgdn1942@gmail.com>
2017-12-02 14:43:58 +0300

    Update to build twrp_3.2.0 (compatibility only 6.x+ roms)

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -153,11 +153,7 @@ RECOVERY_GRAPHICS_USE_LINELENGTH := true
 TARGET_PREBUILT_RECOVERY_KERNEL := $(LOCAL_PATH)/kernel
 TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/devices/platform/mt_usb/musb-hdrc.0.auto/gadget/lun%d/file"
 
-ifeq ($(RECOVERY_VARIANT),twrp)
-TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/root/twrp.fstab
-else
-TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/root/recovery.fstab
-endif
+TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/root/fstab.mt6580
 
 # TWRP
 ifeq ($(RECOVERY_VARIANT),twrp)

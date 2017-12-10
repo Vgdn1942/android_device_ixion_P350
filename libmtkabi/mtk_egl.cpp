135644f50accece8c5ca88f6e68c32a6ab0a079d
vgdn1942 <vgdn1942@gmail.com>
2017-12-11 00:18:29 +0300

    Add symbol for camera

--- libmtkabi/mtk_egl.cpp
+++ libmtkabi/mtk_egl.cpp
@@ -4,4 +4,5 @@ extern "C" {
     void _ZN7android22IGraphicBufferConsumer10BufferItemC1Ev () {}
     void _ZN7android18BufferItemConsumer20setDefaultBufferSizeEjj () {}
     void _ZN7android18BufferItemConsumer22setDefaultBufferFormatEj () {}
+    void _ZN7android18BufferItemConsumer13acquireBufferEPNS_22IGraphicBufferConsumer10BufferItemExb () {}
 }

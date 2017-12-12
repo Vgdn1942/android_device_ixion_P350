ca96123873a99d83ebeeab8f50605897d2a3bc12
vgdn1942 <vgdn1942@gmail.com>
2017-12-12 23:48:20 +0300

    Add shim for camera

--- libmtkabi/mtk_egl.cpp
+++ libmtkabi/mtk_egl.cpp
@@ -5,4 +5,5 @@ extern "C" {
     void _ZN7android18BufferItemConsumer20setDefaultBufferSizeEjj () {}
     void _ZN7android18BufferItemConsumer22setDefaultBufferFormatEj () {}
     void _ZN7android18BufferItemConsumer13acquireBufferEPNS_22IGraphicBufferConsumer10BufferItemExb () {}
+    void _ZN7android18BufferItemConsumer13releaseBufferERKNS_22IGraphicBufferConsumer10BufferItemERKNS_2spINS_5FenceEEE () {}
 }
